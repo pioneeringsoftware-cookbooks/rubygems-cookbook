5cfad5c992d414a17b6735dda59662e12eeb66ad
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2015-02-04 11:14:15 +0000

    Ruby and RubyGems version bump

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,14 @@
 # You can update the tarball checksums on OS X using shasum -a 256 followed by
 # the path of the tarballs. Or, alternatively, use Curl as follows.
 #
-#   curl http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p385.tar.gz | shasum -a 256
-#   curl http://http://production.cf.rubygems.org/rubygems/rubygems-1.8.25.tgz | shasum -a 256
+#   curl http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz | shasum -a 256
+#   curl http://production.cf.rubygems.org/rubygems/rubygems-2.4.5.tgz | shasum -a 256
 #
 
-default['ruby']['version'] = '1.9.3-p385'
-default['ruby']['checksum'] = '4b15df007f5935ec9696d427d8d6265b121d944d237a2342d5beeeba9b8309d0'
+default['ruby']['version'] = '2.2.0'
+default['ruby']['checksum'] = '7671e394abfb5d262fbcd3b27a71bf78737c7e9347fa21c39e58b0bb9c4840fc'
 default['ruby']['prefix'] = '/usr'
 default['ruby']['make_opts'] = '-j10'
 
-default['rubygems']['version'] = '1.8.25'
-default['rubygems']['checksum'] = '649348ddf8746887fb1ee79c55dc508f0627d3d0bfa7fcdbcd4edb24908f1cc8'
+default['rubygems']['version'] = '2.4.5'
+default['rubygems']['checksum'] = '47d182ba52da02d4400601efbf62f64c25ff83856f8e269f8289333f292566d9'
